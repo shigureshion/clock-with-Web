ff211f01b4c0af8ea210a64759b0d64499c9efe2
shigureshion <magnolia19990129@gmail.com>
2021-06-30 14:45:52 +0900

    更新

--- clock.js
+++ clock.js
@@ -1,16 +1,25 @@
 function clockjs(){
     var nowtime = new Date();
-    var nowhour = nowtime.getHours();
-    var nowmin = nowtime.getMinutes();
-    var nowsec = nowtime.getSeconds();
+    var nowhour = set2fig(nowtime.getHours());
+    var nowmin = set2fig(nowtime.getMinutes());
+    var nowsec = set2fig(nowtime.getSeconds());
 
     var clockontime = nowhour + ":" + nowmin + ":" + nowsec;
     document.getElementById("clock").innerHTML = clockontime;
 
     var nowyear = nowtime.getFullYear();
-    var nowmonth = nowtime.getMonth();
-    var nowday = nowtime.getDay();
+    var nowmonth = nowtime.getMonth() + 1 ;
+    var nowday = nowtime.getDate();
 
     var clockonday = nowyear + "年" + nowmonth + "月" + nowday + "日";
     document.getElementById("day").innerHTML = clockonday;
+}
+
+setInterval('clockjs()',1000);
+
+function set2fig(t){
+    var set;
+    if(t < 10){set = "0" + t;}
+    else{set = t;}
+    return set;
 }
